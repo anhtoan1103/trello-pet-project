b078c529d3e86f4a61718d9c2c00a32066eab8d2
Tra Anh Toan <toantra732@gmail.com>
2023-07-27 22:54:07 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import Button from '@mui/material/Button'
+import HomeIcon from '@mui/icons-material/Home'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>toto</div>
+      <Button variant='contained'>Hello world</Button>
+      <Button variant='text'>Hello world</Button>
+      <Button variant='outlined'>Hello world</Button>
+
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <HomeIcon color="primary" />
     </>
   )
 }
