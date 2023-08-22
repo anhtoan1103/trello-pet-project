4efe51e5686acf389bd895ec1fcdc226e5bf4a03
Tra Anh Toan <toantra732@gmail.com>
2023-08-22 13:14:01 +0700

    add basic layout and basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-import Button from '@mui/material/Button'
-import HomeIcon from '@mui/icons-material/Home'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 import Box from '@mui/material/Box'
-// import useMediaQuery from '@mui/material/useMediaQuery'
+import Container from '@mui/material/Container'
+
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
   const handleChange = (event) => {
@@ -50,42 +46,38 @@ function ModeSelect() {
   )
 }
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  // const preferDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  // const preferLightMode = useMediaQuery('(prefers-color-scheme: light)')
-  // console.log('preferDarkMode:', preferDarkMode)
-  // console.log('preferLightMode:', preferLightMode)
-  return (
-    <>
-    <Button 
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-    </>
-  )
-}
-
 function App() {
   return (
     <>
-      <ModeSelect></ModeSelect>
-      <hr></hr>
-      <ModeToggle />
-        <br />
-        <div>toto</div>
+      <Container disableGutters maxWidth={false} sx={{ height: '100vh'}}>
+        <Box sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          <ModeSelect></ModeSelect>
+        </Box>
+        <Box sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: (theme) => theme.trello.boardBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}>
+          Board Bar
+        </Box>
+        <Box sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`
+        }}>
 
-        <Typography variant='body2' color="text.secondary">Test Typograpy</Typography>
-        <Button variant='contained'>Hello world</Button>
-        <Button variant='text' color='success'>Hello world</Button>
-        <Button variant='outlined'>Hello world</Button>
-        <br />
-        <AccessAlarmIcon />
-        <ThreeDRotation />
-        <HomeIcon color="primary" />
+        </Box>
+      </Container>
     </>
   )
 }
