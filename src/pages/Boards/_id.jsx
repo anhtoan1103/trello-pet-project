f4e123d0b206e8c83a9a7adb25217464ee2cf584
Tra Anh Toan <toantra732@gmail.com>
2023-08-25 14:13:25 +0700

    relative and absoulute import fix

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,7 +2,7 @@
 import Container from '@mui/material/Container'
 
 import AppBar from '~/components/AppBar'
-import BoardBar from './BoardBar'
+import BoardBar from '~/BoardBar'
 import BoardContent from './BoardContent'
 
 function Board() {
